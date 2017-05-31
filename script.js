5bda6bd5317a7af6be43e58bd4089721beb8dec0
jenniferwiner <jeniwiner@gmail.com>
2017-05-31 09:29:50 -0600

    loader

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function onReady(callback) {
+  let intervalID = window.setInterval(checkReady, 1000)
+  function checkReady() {
+    if ($('body')[0]) {
+      window.clearInterval(intervalID)
+      callback.call(this)
+    }
+  }
+}
 
+function show(id, value) {
+  document.getElementById(id).style.display = value ? 'block' : 'none'
+}
+
+onReady(function() {
+  show('page', true)
+  show('loading', false)
+})
