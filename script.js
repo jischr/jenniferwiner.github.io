35ac821d0b7d1f207e9d5d200cddcdf0eaa50486
jenniferwiner <jeniwiner@gmail.com>
2017-06-02 16:56:18 -0600

    slide down animation

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-function onReady(callback) {
-  let intervalID = window.setInterval(checkReady, 1000)
-  function checkReady() {
-    if ($('body')[0]) {
-      window.clearInterval(intervalID)
-      callback.call(this)
+$(document).ready(function() {
+  function onReady(callback) {
+    let intervalID = window.setInterval(checkReady, 500)
+    function checkReady() {
+      if ($('body')[0]) {
+        window.clearInterval(intervalID)
+        callback.call(this)
+      }
     }
   }
-}
 
-function show(id, value) {
-  document.getElementById(id).style.display = value ? 'block' : 'none'
-}
+  function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none'
+  }
 
-onReady(function() {
-  show('page', true)
-  show('loading', false)
+  onReady(function() {
+    show('page', true)
+    show('loading', false)
+  })
+
+  function navArrowScroll(location) {
+    $('html, body').animate({
+      scrollTop: $(`#${location}`).offset().top
+    }, 1000)
+  }
+
+  $('.nav-arrow').click(function() {
+    navArrowScroll($(event.target).parent().attr('id'))
+  })
 })
