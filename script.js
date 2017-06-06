ed10d28c98de65aab36db717334850161193181e
jenniferwiner <jeniwiner@gmail.com>
2017-06-06 16:19:08 -0600

    compress splash img

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  function onReady(callback) {
-    let intervalID = window.setInterval(checkReady, 500)
-    function checkReady() {
-      if ($('body')[0]) {
-        window.clearInterval(intervalID)
-        callback.call(this)
-      }
-    }
-  }
-
-  function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none'
-  }
-
-  onReady(function() {
-    show('page', true)
-    show('loading', false)
-  })
+  // function onReady(callback) {
+  //   let intervalID = window.setInterval(checkReady, 500)
+  //   function checkReady() {
+  //     if ($('body')[0]) {
+  //       window.clearInterval(intervalID)
+  //       callback.call(this)
+  //     }
+  //   }
+  // }
+  //
+  // function show(id, value) {
+  //   document.getElementById(id).style.display = value ? 'block' : 'none'
+  // }
+  //
+  // onReady(function() {
+  //   show('page', true)
+  //   show('loading', false)
+  // })
 
   function navArrowScroll(location) {
     $('html, body').animate({
